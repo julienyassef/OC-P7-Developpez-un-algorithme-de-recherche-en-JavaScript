61cda1dc17bd7fbb7fa211ddf96d4e87e330032c
julien yassef <juyassef@gmail.com>
2023-10-18 00:28:07 +0200

    function displayFilterIngredients

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -2,87 +2,16 @@ import { getExternalData } from "../utils/getData.js";
 import { createRecipeCard } from "../utils/factories.js";
 import { saveData } from "../utils/storage.js";
 import { displayRecipes } from "../utils/dom.js";
+import { displayFilterIngredients } from "../utils/dom.js";
 
 
 const init = async () => {
     const recipes = await getExternalData();
     saveData(recipes);
     displayRecipes(recipes);
+    displayFilterIngredients(recipes);
 }
 init ();
-
-
-/// ================================================
-/// menu filter: ingredients, appareils et ustensils
-/// ================================================
-
-
-// class MenuFilterFactory {
-//     static createIgrendientsList(ingredients) {
-
-//         for (const ingredient of ingredients) {
-//             const ingredientName = ingredient.ingredient ;
-        
-//             const ingredientPush = document.createElement('p');
-//             ingredientPush.classList.add('menu-filter__list-ingredients__ingredient');
-//             ingredientPush.textContent = ingredientName;
-    
-//             return ingredientPush;
-//           }
-//     }
-//     static createApplianceList(appliance) {
-        
-//         const appliancesPush = document.createElement('p');
-//         appliancesPush.classList.add('menu-filter__list-ingredients__appliance');
-//         appliancesPush.textContent = appliance;
-
-//         return appliancesPush;
-//     }
-//     static createUstensilsList(ustensils) {
-
-//         for (let i = 0; i < ustensils.length; i++)  {
-//             const ustensil = ustensils[i];
-//             const ustensilPush = document.createElement('p');
-//             ustensilPush.classList.add('menu-filter__list-ingredients__ustensils');
-//             ustensilPush.textContent = ustensil;
-
-//             return ustensilPush;
-//           }
-//     }
-// }
-
-// const getDateMenuFilter = async (recipes) => {
-    
-//     // sélectionne les div html pour mettre les donnés
-//     const sectionListIgredients = document.querySelector('.menu-filter__list-ingredients');
-//     const sectionListUstensils = document.querySelector('.menu-filter__list-ustensils');
-//     const sectionListAppliance = document.querySelector('.menu-filter__list-appliance');
-    
-//     for (let i = 0; i < recipes.length; i++) {
-//         const recipe = recipes[i];
-//         const ingredients = recipe.ingredients;
-//         const appliance = recipe.appliance;
-//         const ustensils = recipe.ustensils;
-        
-        
-//         //rajoute les listes des data sur les menus filter
-//         // const dataMenuFilterIngredients = MenuFilterFactory.createIgrendientsList(ingredients);
-//         // sectionListIgredients.appendChild(dataMenuFilterIngredients);
-    
-//         // const dataMenuFilterAppliance= MenuFilterFactory.createApplianceList(appliance);
-//         // sectionListAppliance.appendChild(dataMenuFilterAppliance);
-    
-//         // const dataMenuFilterUstensils = MenuFilterFactory.createUstensilsList(ustensils);
-//         // sectionListUstensils.appendChild(dataMenuFilterUstensils);
-//       }
-
-// };
-//   getDateMenuFilter(recipes);
-
-
-
-
-
 
 
 
--- scripts/utils/dom.js
+++ scripts/utils/dom.js
@@ -19,4 +19,77 @@ export const displayRecipes = (recipes) => {
    
 }
 
+export const displayFilterIngredients = (recipes) => {
+    const sectionListIngredients = document.querySelector('.menu-filter__list-ingredients');
+    sectionListIngredients.innerHTML = "";
+
+    let listIngredients = []; 
+
+    for (let i = 0; i < recipes.length; i++) {
+        const recipe = recipes[i];
+        const ingredients = recipe.ingredients;
+        const dataIngredients = ingredients.map((element) => element.ingredient);
+    
+        // Ajoutez les noms d'ingrédients à la liste
+        listIngredients = listIngredients.concat(dataIngredients);
+    }
+    
+    const uniqueIngredients = new Set(); 
+    
+
+    listIngredients.forEach((ingredientName) => {
+        if (!uniqueIngredients.has(ingredientName)) {
+            uniqueIngredients.add(ingredientName);
+    
+            const ingredientElement = document.createElement('p');
+            ingredientElement.classList.add('menu-filter__list-ingredients__ingredient');
+            ingredientElement.textContent = ingredientName;
+            sectionListIngredients.appendChild(ingredientElement);
+        }
+    });
+}
+
+
+
+//     static createApplianceList(appliance) {
+        
+//         const appliancesPush = document.createElement('p');
+//         appliancesPush.classList.add('menu-filter__list-ingredients__appliance');
+//         appliancesPush.textContent = appliance;
+
+//         return appliancesPush;
+//     }
+//     static createUstensilsList(ustensils) {
+
+//         for (let i = 0; i < ustensils.length; i++)  {
+//             const ustensil = ustensils[i];
+//             const ustensilPush = document.createElement('p');
+//             ustensilPush.classList.add('menu-filter__list-ingredients__ustensils');
+//             ustensilPush.textContent = ustensil;
+
+//             return ustensilPush;
+//           }
+//     }
+// }
+
+// const getDateMenuFilter = async (recipes) => {
+    
+//     // sélectionne les div html pour mettre les donnés
+//     
+//     const sectionListUstensils = document.querySelector('.menu-filter__list-ustensils');
+//     const sectionListAppliance = document.querySelector('.menu-filter__list-appliance');
+    
+//     for (let i = 0; i < recipes.length; i++) {
+//         const recipe = recipes[i];
+//         const ingredients = recipe.ingredients;
+//         const appliance = recipe.appliance;
+//         const ustensils = recipe.ustensils;
+        
+ 
+
+
+
+
+
+
  
