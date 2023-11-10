c4e076068a7ffb36a409bc5fcb7f0d9e0402da4c
julien yassef <juyassef@gmail.com>
2023-11-10 08:56:28 +0100

    suppressin pin

--- scripts/utils/tagDeletionManagement.js
+++ scripts/utils/tagDeletionManagement.js
@@ -10,6 +10,21 @@ export const tagDeletionManagement = (tag) => {
     // Supprime le tag sélectionné
     tag.remove();
 
+    // modifie l'affichage des recipes lorsqu'on supprime un tag
     filterRecipesDeleteByTag(tag);
-  
-};
+
+
+    // supprime le Pin afficher dans les menus filter
+    const selectPinIngredients = Array.from(document.querySelectorAll(".menu-filter__container-filter__menu__section-pin-ingredients__pinElement"));
+    const selectPinAppliance = Array.from(document.querySelectorAll(".menu-filter__container-filter__menu__section-pin-appliance__pinElement"));
+    const selectPinUstensils = Array.from(document.querySelectorAll(".menu-filter__container-filter__menu__section-pin-ustensils__pinElement"));
+    const selectPinDisplay = [...selectPinIngredients, ...selectPinAppliance, ...selectPinUstensils];
+
+
+    selectPinDisplay.forEach(pin => {
+        if (pin.textContent.toLowerCase() === tag.textContent.toLowerCase()) {
+            pin.remove();
+        }
+        
+    });
+}
