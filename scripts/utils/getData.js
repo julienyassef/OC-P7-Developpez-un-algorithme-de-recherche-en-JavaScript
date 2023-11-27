d8ff7157c359502a91c70b806018e955613d1b2e
julien yassef <juyassef@gmail.com>
2023-11-27 17:26:47 +0100

    modif alogTwo getData

--- scripts/utils/getData.js
+++ scripts/utils/getData.js
@@ -2,10 +2,13 @@ export const getExternalData = async () => {
     
     const data = await fetch('../../data/recipes.json')
     const result = await data.json();
-     return result.map(recipes => {
+    result.map(recipes => {
         recipes.display = true;
     });
+
+   return result
     }
 
 
 
+  
